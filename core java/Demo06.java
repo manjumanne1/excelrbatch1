1417dcdf8ea27b0f7861310251e507993ac263e8
manjumanne1 <mannechandra95@gmail.com>
2025-01-21 12:58:40 +0530

    Username and Password

--- core java/Demo06.java
+++ core java/Demo06.java
@@ -1,8 +1,8 @@
 public class Demo6 {
     public static void main(String[] args) {
-        String username = "narendra";
-        String pwd = "nare@8247";
-        while((username == "narendra") && (pwd == "nare@8247")) {
+        String username = "manju";
+        String pwd = "manju@8247";
+        while((username == "manju") && (pwd == "manju@8247")) {
             System.out.println("Welcome to mallareddy university");
             break;
         }
