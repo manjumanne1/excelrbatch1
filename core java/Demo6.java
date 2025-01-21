9212f8bb1a0c7a36d3a9105c1af0d123cb8314cf
manjumanne1 <mannechandra95@gmail.com>
2025-01-21 11:32:27 +0530

    Add files via upload

---
+++ core java/Demo6.java
@@ -0,0 +1,10 @@
+public class Demo6 {
+    public static void main(String[] args) {
+        String username = "narendra";
+        String pwd = "nare@8247";
+        while((username == "narendra") && (pwd == "nare@8247")) {
+            System.out.println("Welcome to mallareddy university");
+            break;
+        }
+    }
+}
